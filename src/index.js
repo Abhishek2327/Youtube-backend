1a29d615907a572932eaaa27d64f3795c51376ab
Abhishek2327 <abhishek202327@gmail.com>
2026-04-28 20:18:05 +0530

    connected to moongo atlas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+//import('dotenv').config({path: './env' })
+import dotenv from "dotenv"
+ 
+ 
+import connectdb from "./db/index.js";
+
+ dotenv.config({
+    path: './env'
+ })
+
+connectdb();
+
+
+/*
+import mogoose, { connect } from "mongoose"
+import { DB_NAME } from "./constants";
+import express from "express"
+const app=express()
+
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error" , (error)=>{
+            console.error("errr",error)
+            throw error
+        })
+
+         app.listen(process.env.PORT,()=>{
+            console.log(`app is listening oon port:${process.env.PORT}`);
+         })
+        
+    } catch (error) {
+        console.error("ERROR:",error)
+        throw error
+    }
+})()
+*/
